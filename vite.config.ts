312e6068c28c8e02e5c4b6a73b38683ac11229d7
Citlalli Del Moral <citlallidmg@gmail.com>
2025-05-07 11:40:05 -0600

    update: adds deploy configuration to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://citlallidmg.github.io/qventus-th-custom-password-input",
+});
