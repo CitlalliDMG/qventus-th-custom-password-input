6ea99a7d72a10c896617bda8ed71dca52ffe67fc
Citlalli Del Moral <citlallidmg@gmail.com>
2025-05-07 11:34:20 -0600

    update: adds validation container with logic

--- src/components/PasswordField.tsx
+++ src/components/PasswordField.tsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
+import Icon from "react-icons-kit";
+import { ic_cancel } from "react-icons-kit/md/ic_cancel";
+import { ic_check_circle } from "react-icons-kit/md/ic_check_circle";
 import { PasswordOptions } from "../types/passwordFieldTypes";
+import {
+  availablePasswordValidations,
+  passwordValidations,
+} from "../utils/passwordValidations";
 
 function PasswordField({
   options,
@@ -12,13 +19,40 @@ function PasswordField({
 }) {
   const [password, setPassword] = useState("");
 
+  const validations = passwordValidations(
+    options,
+    availablePasswordValidations
+  );
+
   return (
     <div className="wrapper">
       <div className="password-input">
-        <label style={{ display: showLabel ? "block" : "none" }}>{label}</label>
-        <input type="text" onChange={(e) => setPassword(e.target.value)} />
+        <input
+          type="text"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder={showLabel ? label : ""}
+        />
       </div>
-      <div className="password-validations">test</div>
+      <div className="password-validations">
+        {validations.map((validation) => {
+          const { validationId, validationText, validator } = validation;
+
+          return (
+            <div className="validation" key={validationId}>
+              {validator(password.trim()) ? (
+                <span className="validation-icon green">
+                  <Icon size={39} icon={ic_check_circle} />
+                </span>
+              ) : (
+                <span className="validation-icon red">
+                  <Icon size={39} icon={ic_cancel} />
+                </span>
+              )}
+              {validationText}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
